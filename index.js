ca6d730a96489f6a77de77b8fb35944b73c7a359
Krrishika Rana <krrishikarana83@gmail.com>
2026-03-06 23:52:13 +0530

    fixed ES module error

--- index.js
+++ index.js
@@ -1,26 +1,8 @@
-// Node.js is a JavaScript runtime built on Chrome's V8 JavaScript engine. 
-// It allows developers to run JavaScript code on the server side, enabling the creation of scalable 
-// and high-performance web applications. Node.js uses an event-driven, non-blocking I/O model, 
-// making it efficient for handling concurrent requests and real-time applications. 
-// It has a vast ecosystem of libraries and frameworks, such as Express.js, which simplifies 
-// the development of web servers and APIs.
+import express from "express";
+import app from "./app.js";
 
-// Node.js entry point for the application. This file is responsible for starting the server and 
-// connecting to necessary services like Redis.
+const PORT = process.env.PORT || 3000;
 
-// Importing mechanism - ES6 Modules (import/export) is a standardized module system in JavaScript that allows 
-// developers to organize and reuse code across different files and modules. It provides a clean and intuitive 
-// syntax for importing and exporting functions, objects, or values between modules. In this file, we use the
-// import statement to bring in the Express application instance from the app.js file and the connectRedis function
-// from the Redis configuration file. This allows us to set up our server and establish a connection to Redis
-// before starting to listen for incoming requests.
-const express = require("express");
-const app = express();
-
-app.get("/", (req, res) => {
-  res.send("Redis Locking Mechanism API is running 🚀");
-});
-
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
